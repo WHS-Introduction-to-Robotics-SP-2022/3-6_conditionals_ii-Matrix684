c5173aca34ad1e3032f0d8eae3a6bf8a0be9c3a3
Matrix684 <99276073+Matrix684@users.noreply.github.com>
2022-04-08 09:36:04 -0600

    Create 3.7_analog_input_control.ino

---
+++ 3.7_analog_input_control.ino
@@ -0,0 +1,51 @@
+int Red=11;
+int Green=10;
+int Blue=9;
+
+int inPin=A0;
+int Val;
+
+void setup() {
+  pinMode(inPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(Val);
+  Val=analogRead(inPin);
+  if (Val<=146) {
+  analogWrite(Red, 255);
+  analogWrite(Green, 0);
+  analogWrite(Blue, 0);
+  }
+  else if (Val>146 && Val<=292) {
+  analogWrite(Red, 235);
+  analogWrite(Green, 5);
+  analogWrite(Blue, 0);
+  }
+  else if (Val>292 && Val<=438) {
+  analogWrite(Red, 255);
+  analogWrite(Green, 35);
+  analogWrite(Blue, 0);
+  }
+  else if (Val>438 && Val<=584) {
+  analogWrite(Red, 0);
+  analogWrite(Green, 255);
+  analogWrite(Blue, 0);
+  }
+  else if (Val>584 && Val<=730) {
+  analogWrite(Red, 0);
+  analogWrite(Green, 35);
+  analogWrite(Blue, 255);
+  }
+  else if (Val>730 && Val<=876) {
+  analogWrite(Red, 0);
+  analogWrite(Green, 0);
+  analogWrite(Blue, 255);
+  }
+  else {
+  analogWrite(Red, 255);
+  analogWrite(Green, 0);
+  analogWrite(Blue, 255);
+  }
+}
